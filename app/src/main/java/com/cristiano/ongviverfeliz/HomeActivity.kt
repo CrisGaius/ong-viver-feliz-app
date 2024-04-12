0fe8945fe0f1895657d3c9c4fb35b8f4ceb6b9db
AislanJunior <aislandnjunior@gmail.com>
2024-04-12 01:37:11 -0300

    Adicionada função de login e logout.

--- app/src/main/java/com/cristiano/ongviverfeliz/HomeActivity.kt
+++ app/src/main/java/com/cristiano/ongviverfeliz/HomeActivity.kt
@@ -1,20 +1,41 @@
 package com.cristiano.ongviverfeliz
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cristiano.ongviverfeliz.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 class HomeActivity : AppCompatActivity() {
+
+    private val auth by lazy{
+        FirebaseAuth.getInstance()
+    }
+    private val binding by lazy{
+        ActivityHomeBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.btnLogout.setOnClickListener {
+            deslogarUsuario()
         }
+    }
+
+    private fun deslogarUsuario() {
+        AlertDialog.Builder(this)
+            .setTitle("Deslogar")
+            .setMessage("Deseja realmente sair?")
+            .setNegativeButton("Cancelar"){dialog, posicao -> }
+            .setPositiveButton("Sim"){dialog, posicao ->
+                auth.signOut()
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+            }.create()
+            .show()
     }
 }
--- app/src/main/java/com/cristiano/ongviverfeliz/MainActivity.kt
+++ app/src/main/java/com/cristiano/ongviverfeliz/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.cristiano.ongviverfeliz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cristiano.ongviverfeliz.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.storage.FirebaseStorage
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var email: String
+    private lateinit var senha: String
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val auth by lazy{
+        FirebaseAuth.getInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        inicializarEventosClique()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        verificarUsuarioLogado()
+    }
+
+    private fun verificarUsuarioLogado() {
+        val usuarioAtual = auth.currentUser
+        if(usuarioAtual != null){
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+
+    private fun inicializarEventosClique() {
+        binding.btnLogar.setOnClickListener {
+            if ( validarCampos()){
+                logarUsuario()
+            }
+        }
+    }
+
+    private fun logarUsuario() {
+        auth.signInWithEmailAndPassword(email, senha).addOnSuccessListener {
+            Toast.makeText(this, "Logado com sucesso!", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, HomeActivity::class.java))
+        }.addOnFailureListener {erro ->
+            try {
+                throw erro
+            }catch (erroUsuarioInvalido: FirebaseAuthInvalidUserException){
+                erroUsuarioInvalido.printStackTrace()
+                Toast.makeText(this, "E-mail não cadastrado", Toast.LENGTH_LONG).show()
+            }catch (erroCredenciaisInvalidas: FirebaseAuthInvalidCredentialsException){
+                erroCredenciaisInvalidas.printStackTrace()
+                Toast.makeText(this, "E-mail ou senha estão incorretos!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun validarCampos(): Boolean {
+        email = binding.editLoginEmail.text.toString()
+        senha = binding.editLoginSenha.text.toString()
+
+        if(email.isNotEmpty()){
+            binding.textInputLayoutLoginEmail.error = null
+            if (senha.isNotEmpty()){
+                binding.textInputLayoutLoginSenha.error = null
+                return true
+            }else{
+                binding.textInputLayoutLoginSenha.error = "Preencha a senha"
+                return false
+            }
+        }else{
+            binding.textInputLayoutLoginEmail.error = "Preencha o email"
+            return false
         }
     }
 }
