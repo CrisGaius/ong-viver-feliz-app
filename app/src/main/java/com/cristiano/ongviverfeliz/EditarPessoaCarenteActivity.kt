fe1767179f5a3fed488702294f3ab2545bfc0d19
AislanJunior <aislandnjunior@gmail.com>
2024-04-16 21:11:26 -0300

    feat: adicionando id's para a tela de editar voluntário

--- app/src/main/java/com/cristiano/ongviverfeliz/EditarPessoaCarenteActivity.kt
+++ app/src/main/java/com/cristiano/ongviverfeliz/EditarPessoaCarenteActivity.kt
@@ -1,20 +1,11 @@
 package com.cristiano.ongviverfeliz
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class EditarPessoaCarenteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_editar_pessoa_carente)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
